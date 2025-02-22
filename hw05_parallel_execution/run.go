f6148f1b29c2ccd1df1730f5cf66c82643ba5339
Vitalii Gudza <vitalii.gudza@dif.tech>
2025-02-22 22:19:48 +0200

    HW5: improve code

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -24,7 +24,6 @@ func Run(tasks []Task, n, m int) error {
 	}
 
 	taskChan := make(chan Task)
-	doneChan := make(chan struct{})
 	wg := &sync.WaitGroup{}
 	var errorsCount atomic.Int64
 
@@ -37,33 +36,22 @@ func Run(tasks []Task, n, m int) error {
 			defer wg.Done()
 
 			for task := range taskChan {
-				err := task()
-				if err != nil {
-					if errorsCount.Add(1) >= int64(m) {
-						select {
-						case doneChan <- struct{}{}:
-						default:
-						}
-
-						return
-					}
+				if err := task(); err != nil {
+					errorsCount.Add(1)
 				}
 			}
 		}()
 	}
 
-outer:
 	for _, task := range tasks {
-		select {
-		case taskChan <- task:
-		case <-doneChan:
-			break outer
+		taskChan <- task
+		if errorsCount.Load() >= int64(m) {
+			break
 		}
 	}
-	close(taskChan)
 
+	close(taskChan)
 	wg.Wait()
-	close(doneChan)
 
 	if errorsCount.Load() >= int64(m) {
 		return ErrErrorsLimitExceeded
@@ -71,71 +59,3 @@ outer:
 
 	return nil
 }
-
-// solution with context
-// func Run(tasks []Task, n, m int) error {
-// 	if m <= 0 {
-// 		return ErrErrorsLimitExceeded
-// 	}
-//
-// 	if n <= 0 {
-// 		return ErrInvalidWorkersCount
-// 	}
-//
-// 	taskChan := make(chan Task)
-// 	wg := &sync.WaitGroup{}
-// 	var errorsCount atomic.Int64
-//
-// 	ctx, cancel := context.WithCancel(context.TODO())
-// 	defer cancel()
-//
-//  workersCount := min(n, len(tasks))
-// 	wg.Add(workersCount)
-//
-// 	for i := 0; i < workersCount; i++ {
-// 		go func() {
-// 			defer wg.Done()
-//
-// 			for {
-// 				select {
-// 				case task, ok := <-taskChan:
-// 					if !ok {
-// 						return
-// 					}
-//
-// 					err := task()
-// 					if err != nil {
-// 						if errorsCount.Add(1) >= int64(m) {
-// 							cancel()
-// 							return
-// 						}
-// 					}
-//
-// 				case <-ctx.Done():
-// 					return
-// 				}
-// 			}
-//
-// 		}()
-// 	}
-//
-// 	go func() {
-// 		defer close(taskChan)
-//
-// 		for _, task := range tasks {
-// 			select {
-// 			case taskChan <- task:
-// 			case <-ctx.Done():
-// 				return
-// 			}
-// 		}
-// 	}()
-//
-// 	wg.Wait()
-//
-// 	if errorsCount.Load() >= int64(m) {
-// 		return ErrErrorsLimitExceeded
-// 	}
-//
-// 	return nil
-// }
