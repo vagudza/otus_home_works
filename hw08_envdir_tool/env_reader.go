46e22872ea77c093933cc80c06d0577c2e12ee78
Vitalii Gudza <vitalii.gudza@dif.tech>
2025-04-15 00:35:11 +0300

    HW8 is done

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,41 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment)
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+
+		// Skip files with '=' in their name to avoid conflicts with environment variable names (FOO=BAR=value)
+		name := f.Name()
+		if strings.Contains(name, "=") {
+			continue
+		}
+
+		content, err := os.ReadFile(filepath.Join(dir, name))
+		if err != nil {
+			return nil, err
+		}
+
+		// split the content by new line and take the first line
+		lines := bytes.SplitN(content, []byte("\n"), 2)
+		// replace all null bytes in first line with new line
+		firstLine := bytes.ReplaceAll(lines[0], []byte{0x00}, []byte("\n"))
+		// clear all trailing spaces and tabs
+		value := strings.TrimRight(string(firstLine), " \t")
+
+		env[name] = EnvValue{
+			Value:      value,
+			NeedRemove: len(content) == 0,
+		}
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,32 @@
 package main
 
+import "os"
+
 func main() {
-	// Place your code here.
+	// os.Args = [
+	//    "./go-envdir",  // [0] — this program name
+	//    "/path/to/envdir",  // [1] — envdir directory
+	//    "echo",  // [2] — command to run
+	//    "Hello World"  // [3] — command arguments
+	// ]
+
+	if len(os.Args) < 3 {
+		_, _ = os.Stderr.WriteString("Usage: go-envdir <envdir> <cmd> [args...]\n")
+		// https://www.unix.com/man_page/debian/8/envdir/
+		// envdir exits 111 if it has trouble reading d, if it runs out of memory for environment variables,
+		// or if it cannot run child.  Otherwise its exit code is the same as that of child
+		os.Exit(111)
+	}
+
+	envDir := os.Args[1]
+	cmd := os.Args[2:]
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		_, _ = os.Stderr.WriteString("Error reading env dir: " + err.Error() + "\n")
+		os.Exit(111)
+	}
+
+	exitCode := RunCmd(cmd, env)
+	os.Exit(exitCode)
 }
