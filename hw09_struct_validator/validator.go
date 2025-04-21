3e91888ac6f688ace3db8a13186db0713f2dc712
Vitalii Gudza <vitalii.gudza@dif.tech>
2025-04-21 15:49:51 +0300

    [HW9] is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,259 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
 }
 
+func (v ValidationError) Error() string {
+	return fmt.Sprintf("field %s: %v", v.Field, v.Err)
+}
+
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var sb strings.Builder
+	for i, err := range v {
+		sb.WriteString(err.Error())
+		if i < len(v)-1 {
+			sb.WriteString(", ")
+		}
+	}
+	return sb.String()
 }
 
+var (
+	ErrNotStruct         = errors.New("input is not a struct")
+	ErrValidateRule      = errors.New("invalid validation rule")
+	ErrRegexpCompile     = errors.New("failed to compile regexp")
+	ErrInvalidType       = errors.New("unsupported type for validation")
+	ErrStringLength      = errors.New("string length mismatch")
+	ErrStringMinLength   = errors.New("string length is less than min length")
+	ErrStringMaxLength   = errors.New("string length is greater than max length")
+	ErrStringRegexp      = errors.New("string does not match regexp")
+	ErrStringNotInSet    = errors.New("string is not in allowed set")
+	ErrNumberMin         = errors.New("number is less than min")
+	ErrNumberMax         = errors.New("number is greater than max")
+	ErrNumberNotInSet    = errors.New("number is not in allowed set")
+	ErrInvalidNestedType = errors.New("invalid nested type")
+)
+
 func Validate(v interface{}) error {
-	// Place your code here.
+	val := reflect.ValueOf(v)
+	if val.Kind() != reflect.Struct {
+		return ValidationErrors{
+			ValidationError{
+				Field: "", // no field name for top-level error
+				Err:   ErrNotStruct,
+			},
+		}
+	}
+
+	var valErrors ValidationErrors
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := typ.Field(i)
+		if !fieldType.IsExported() {
+			continue
+		}
+
+		validateTag := fieldType.Tag.Get("validate")
+		if validateTag == "" {
+			continue
+		}
+
+		if validateTag == "nested" {
+			valErrors = append(valErrors, handleNestedField(field, fieldType)...)
+			continue
+		}
+
+		rules := strings.Split(validateTag, "|")
+		fieldErrs := validateField(field, fieldType.Name, rules)
+		valErrors = append(valErrors, fieldErrs...)
+	}
+
+	if len(valErrors) > 0 {
+		return valErrors
+	}
+
+	return nil
+}
+
+func handleNestedField(
+	field reflect.Value,
+	fieldType reflect.StructField,
+) ValidationErrors {
+	var valErrors ValidationErrors
+
+	if field.Kind() == reflect.Struct {
+		if err := Validate(field.Interface()); err != nil {
+			var nestedValErrors ValidationErrors
+			if errors.As(err, &nestedValErrors) {
+				for _, e := range nestedValErrors {
+					valErrors = append(valErrors, ValidationError{
+						Field: fieldType.Name + "." + e.Field,
+						Err:   e.Err,
+					})
+				}
+			}
+		}
+	} else {
+		valErrors = append(valErrors, ValidationError{
+			Field: fieldType.Name,
+			Err:   ErrInvalidNestedType,
+		})
+	}
+
+	return valErrors
+}
+
+func validateField(field reflect.Value, fieldName string, rules []string) ValidationErrors {
+	var valErrors ValidationErrors
+
+	if field.Kind() == reflect.Slice {
+		for i := 0; i < field.Len(); i++ {
+			elemErrs := validateValue(field.Index(i), fieldName, rules)
+			for _, err := range elemErrs {
+				err.Field = fmt.Sprintf("%s[%d]", err.Field, i)
+				valErrors = append(valErrors, err)
+			}
+		}
+		return valErrors
+	}
+
+	return validateValue(field, fieldName, rules)
+}
+
+func validateValue(value reflect.Value, fieldName string, rules []string) ValidationErrors {
+	var valErrors ValidationErrors
+
+	for _, rule := range rules {
+		parts := strings.SplitN(rule, ":", 2)
+		if len(parts) != 2 {
+			valErrors = append(valErrors, ValidationError{
+				Field: fieldName,
+				Err:   ErrValidateRule,
+			})
+			continue
+		}
+
+		ruleName := parts[0]
+		ruleParam := parts[1]
+
+		//nolint:exhaustive // We are not validating all possible types here, just the ones we need.
+		switch value.Kind() {
+		case reflect.String:
+			err := validateString(value.String(), ruleName, ruleParam)
+			if err != nil {
+				valErrors = append(valErrors, ValidationError{Field: fieldName, Err: err})
+			}
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			err := validateInt(value.Int(), ruleName, ruleParam)
+			if err != nil {
+				valErrors = append(valErrors, ValidationError{Field: fieldName, Err: err})
+			}
+		default:
+			valErrors = append(valErrors, ValidationError{
+				Field: fieldName,
+				Err:   ErrInvalidType,
+			})
+		}
+	}
+
+	return valErrors
+}
+
+func validateString(value string, ruleName string, ruleParam string) error {
+	switch ruleName {
+	case "len":
+		length, err := strconv.Atoi(ruleParam)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrValidateRule, err)
+		}
+		if len(value) != length {
+			return ErrStringLength
+		}
+	case "regexp":
+		re, err := regexp.Compile(ruleParam)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrRegexpCompile, err)
+		}
+		if !re.MatchString(value) {
+			return ErrStringRegexp
+		}
+	case "in":
+		allowedValues := strings.Split(ruleParam, ",")
+		for _, allowed := range allowedValues {
+			if value == allowed {
+				return nil
+			}
+		}
+		return ErrStringNotInSet
+	case "minlength":
+		minLength, err := strconv.Atoi(ruleParam)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrValidateRule, err)
+		}
+		if len(value) < minLength {
+			return ErrStringMinLength
+		}
+	case "maxlength":
+		maxLength, err := strconv.Atoi(ruleParam)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrValidateRule, err)
+		}
+		if len(value) > maxLength {
+			return ErrStringMaxLength
+		}
+	default:
+		return fmt.Errorf("%w: unknown rule %s", ErrValidateRule, ruleName)
+	}
+	return nil
+}
+
+func validateInt(value int64, ruleName string, ruleParam string) error {
+	switch ruleName {
+	case "min":
+		minValue, err := strconv.ParseInt(ruleParam, 10, 64)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrValidateRule, err)
+		}
+		if value < minValue {
+			return ErrNumberMin
+		}
+	case "max":
+		maxValue, err := strconv.ParseInt(ruleParam, 10, 64)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrValidateRule, err)
+		}
+		if value > maxValue {
+			return ErrNumberMax
+		}
+	case "in":
+		allowedValues := strings.Split(ruleParam, ",")
+		for _, allowed := range allowedValues {
+			allowedInt, err := strconv.ParseInt(allowed, 10, 64)
+			if err != nil {
+				return fmt.Errorf("%w: %w", ErrValidateRule, err)
+			}
+			if value == allowedInt {
+				return nil
+			}
+		}
+		return ErrNumberNotInSet
+	default:
+		return fmt.Errorf("%w: unknown rule %s", ErrValidateRule, ruleName)
+	}
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,11 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -11,17 +14,29 @@ type UserRole string
 // Test the function on different structures and other types.
 type (
 	User struct {
-		ID     string `json:"id" validate:"len:36"`
-		Name   string
-		Age    int             `validate:"min:18|max:50"`
-		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
-		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		ID     string   `json:"id" validate:"len:36"`
+		Name   string   `validate:"minlength:3|maxlength:50"`
+		Age    int      `validate:"min:18|max:50"`
+		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role   UserRole `validate:"in:admin,stuff"`
+		Phones []string `validate:"len:11"`
+		meta   json.RawMessage
 	}
 
 	App struct {
-		Version string `validate:"len:5"`
+		Version        string `validate:"len:5"`
+		Database       `validate:"nested"`
+		BusinessConfig `validate:"nested"`
+	}
+
+	Database struct {
+		Host string `validate:"minlength:10|maxlength:50"`
+		Port int    `validate:"min:1|max:65535"`
+	}
+
+	BusinessConfig struct {
+		Code    string `validate:"regexp:^\\d+$"`
+		Product string `validate:"in:debit_card,credit_card"`
 	}
 
 	Token struct {
@@ -38,23 +53,105 @@ type (
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
+		name        string
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			name: "success validation",
+			in: User{
+				ID:     "012345678901234567890123456789012345",
+				Name:   "John",
+				Age:    25,
+				Email:  "john@example.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "12345678901"},
+				meta:   json.RawMessage("{}"),
+			},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			name: "validation with errors",
+			in: User{
+				ID:     "123",                 // wrong length
+				Name:   "J",                   // name is too short
+				Age:    16,                    // age < 18
+				Email:  "johnexample.com",     // incorrect email format
+				Role:   "guest",               // not in allowed roles
+				Phones: []string{"123", "45"}, // wrong length
+				meta:   json.RawMessage("{}"),
+			},
+			expectedErr: ValidationErrors{
+				{Field: "ID", Err: ErrStringLength},
+				{Field: "Name", Err: ErrStringMinLength},
+				{Field: "Age", Err: ErrNumberMin},
+				{Field: "Email", Err: ErrStringRegexp},
+				{Field: "Role", Err: ErrStringNotInSet},
+				{Field: "Phones[0]", Err: ErrStringLength},
+				{Field: "Phones[1]", Err: ErrStringLength},
+			},
+		},
+		{
+			name:        "validate struct with no tags",
+			in:          Token{},
+			expectedErr: nil,
+		},
+		{
+			name: "validate in for int",
+			in:   Response{Code: 300, Body: "OK"},
+			expectedErr: ValidationErrors{
+				{Field: "Code", Err: ErrNumberNotInSet},
+			},
+		},
+		{
+			name: "validate nested struct",
+			in: App{
+				Version: "1.0",
+				Database: Database{
+					Host: "local",
+					Port: 8080,
+				},
+				BusinessConfig: BusinessConfig{
+					Code:    "12345ABC",
+					Product: "unknown",
+				},
+			},
+			expectedErr: ValidationErrors{
+				{Field: "Version", Err: ErrStringLength},
+				{Field: "Database.Host", Err: ErrStringMinLength},
+				{Field: "BusinessConfig.Code", Err: ErrStringRegexp},
+				{Field: "BusinessConfig.Product", Err: ErrStringNotInSet},
+			},
+		},
+		{
+			name:        "not a struct",
+			in:          "not a struct",
+			expectedErr: ErrNotStruct,
+		},
 	}
 
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
 			t.Parallel()
+			err := Validate(tt.in)
 
-			// Place your code here.
-			_ = tt
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+				return
+			}
+
+			var resultErrors ValidationErrors
+			require.True(t, errors.As(err, &resultErrors))
+
+			var specificErrs ValidationErrors
+			if errors.As(tt.expectedErr, &specificErrs) {
+				require.Len(t, resultErrors, len(specificErrs))
+
+				for j := range specificErrs {
+					require.Equal(t, specificErrs[j].Field, resultErrors[j].Field)
+					require.Equal(t, specificErrs[j].Err, resultErrors[j].Err)
+				}
+			}
 		})
 	}
 }
